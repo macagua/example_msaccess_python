354e735c41458be72c42f535aa51aa42d48df56e
Leonardo Caballero <leonardocaballero@gmail.com>
2023-04-28 09:14:03 -0400

    Added the Program to delete the database file for pyodbc library

---
+++ pyodbc/delete_database.py
@@ -0,0 +1,18 @@
+""" Program to delete the database file """
+
+import os
+
+
+DB_PATH = os.path.dirname(
+    os.path.abspath(__file__)
+) + os.sep + "data" + os.sep
+DB_FILE = 'database.accdb'
+DB = DB_PATH + DB_FILE
+
+# Delete Microsoft Access file
+if os.path.exists(DB):
+    print(f"\nExists the Microsoft Access database '{DB_FILE}' file!")
+    os.remove(DB)
+    print(f"\nDelete Microsoft Access database '{DB_FILE}' file!")
+else:
+    print(f"\nDon't exists the Microsoft Access database '{DB_FILE}' file!")
